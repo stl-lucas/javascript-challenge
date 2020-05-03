ad100672efea55ed56178ffe687722b00fe2a0f1
stl-lucas <B5xLcywuG@^#K2c%Wm>
2020-05-03 16:03:29 -0500

    Finished level 2, added remove tbody functionality to reset table on each query, added no results message when no results are found.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,22 +10,42 @@ function handleClick() {
     // prevent page from refreshing
     d3.event.preventDefault();
 
+    // locate table and tbody
+    var table = d3.select("table");
+    var tbody = d3.select("tbody");
+
+    // dynamically remove table rows using new data
+    tbody.remove();
+    table.append("tbody");
+
     // select datetime input from form
     var inputDate = d3.select("#datetime").property("value");
 
     // filter data using the date from the form input
-    var filterData = data.filter(sighting => sighting.datetime ===inputDate);
+    var filterData = data
+    
+    if (inputDate) {
+        filterData = filterData.filter(sighting => sighting.datetime === inputDate);
+    }
 
     // dynamically build table rows using new data
     var tbody = d3.select("tbody");
-    filterData.forEach(sighting => {
+    if (filterData.length > 0) {
+        filterData.forEach(sighting => {
         var row = tbody.append("tr");
         Object.entries(sighting).forEach(([key, value]) => {
             var cell = row.append("td");
             cell.text(value);
-        });
-    });
-};
+            })
+        })
+    }
+    else {
+        var row = tbody.append("tr");
+        var cell = row.append("td");
+        cell.attr("colspan", "7");
+        cell.text("Sorry, no data was found for these filters.");
+    }
+}
 
 button.on("click", handleClick);
 form.on("submit", handleClick);
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -16,25 +16,25 @@ var cityDrop = d3.select("#city");
 cities.forEach(city => {
     var option = cityDrop.append("option")
     option.text(city);
-});
+})
 
 var stateDrop = d3.select("#state");
 states.forEach(state => {
     var option = stateDrop.append("option")
     option.text(state);
-});
+})
 
 var countryDrop = d3.select("#country");
 countries.forEach(country => {
     var option = countryDrop.append("option")
     option.text(country);
-});
+})
 
 var shapesDrop = d3.select("#shape");
 shapes.forEach(shape => {
     var option = shapesDrop.append("option")
     option.text(shape);
-});
+})
 
 // button event listener
 var button = d3.select("button");
@@ -44,6 +44,14 @@ function handleClick() {
 
     // prevent page from refreshing
     d3.event.preventDefault();
+
+    // locate table and tbody
+    var table = d3.select("table");
+    var tbody = d3.select("tbody");
+
+    // dynamically remove table rows using new data
+    tbody.remove();
+    table.append("tbody");
 
     // select datetime input from form
     var inputDate = d3.select("#datetime").property("value");
@@ -73,14 +81,22 @@ function handleClick() {
 
     // dynamically build table rows using new data
     var tbody = d3.select("tbody");
-    filterData.forEach(sighting => {
+    if (filterData.length > 0) {
+        filterData.forEach(sighting => {
         var row = tbody.append("tr");
         Object.entries(sighting).forEach(([key, value]) => {
             var cell = row.append("td");
             cell.text(value);
-        });
-    });
-};
+            })
+        })
+    }
+    else {
+        var row = tbody.append("tr");
+        var cell = row.append("td");
+        cell.attr("colspan", "7");
+        cell.text("Sorry, no data was found for these filters.");
+    }
+}
 
 button.on("click", handleClick);
 form.on("submit", handleClick);
